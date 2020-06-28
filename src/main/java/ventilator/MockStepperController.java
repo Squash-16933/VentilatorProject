83ed0f1cd5862fb531e1d08c82b4573a16ca92c8
daliasebat <daliasebat@192.168.1.4>
2020-06-28 14:21:06 -0700

    pretty formatting ew

--- src/main/java/ventilator/MockStepperController.java
+++ src/main/java/ventilator/MockStepperController.java
@@ -1,29 +1,29 @@
 package ventilator;
 
-
 public class MockStepperController implements StepperInterface {
-@Override
-	 public void forward(int rate, int time){
-	   for (int k = 0; k < rate*time; k++) { // rate*time = #steps in total
-           try {
-               Thread.sleep((int)(1.0 / (rate * 2d) * 1000));// rate = steps per second; 1/rate = seconds per step
-               Thread.sleep((int)(1.0 / (rate * 2d) * 1000));
-               System.out.println("STEP FORWARD");
-           } catch (InterruptedException e) {
-               e.printStackTrace();
-           }
-       }
+	@Override
+	public void forward(int rate, int time) {
+		for (int k = 0; k < rate * time; k++) { // rate*time = #steps in total
+			try {
+				Thread.sleep((int) (1.0 / (rate * 2d) * 1000));// rate = steps per second; 1/rate = seconds per step
+				Thread.sleep((int) (1.0 / (rate * 2d) * 1000));
+				System.out.println("STEP FORWARD");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
 	public void backward(int rate, int time) {
-	 for (int k = 0; k < rate*time; k++) {
-	try {
-        Thread.sleep((int)(1.0 / (rate * 2d) * 1000));// rate = steps per second; 1/rate = seconds per step
-        Thread.sleep((int)(1.0 / (rate * 2d) * 1000));
-        System.out.println("STEP BACKWARD");
-    } catch (InterruptedException e) {
-        e.printStackTrace();
-    }
-	 }
-}}
+		for (int k = 0; k < rate * time; k++) {
+			try {
+				Thread.sleep((int) (1.0 / (rate * 2d) * 1000));// rate = steps per second; 1/rate = seconds per step
+				Thread.sleep((int) (1.0 / (rate * 2d) * 1000));
+				System.out.println("STEP BACKWARD");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
