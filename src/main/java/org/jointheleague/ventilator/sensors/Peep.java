cbabf2c6f424194d62a8233d85184749b0664f19
daliasebat <daliasebat@gmail.com>
2020-11-29 14:34:12 -0800

    this is broken XD (or maybe it isnt who knows)

--- src/main/java/org/jointheleague/ventilator/sensors/Peep.java
+++ src/main/java/org/jointheleague/ventilator/sensors/Peep.java
@@ -6,19 +6,37 @@ import org.jointheleague.ventilator.stepper.MockStepperController;
 import org.jointheleague.ventilator.stepper.StepperController;
 
 public class Peep {
-
+public static final int MAX_PRESSURE = 5; //fix this lol
 	public static void runPeep(float peep) {
 		boolean stop = false;
 		StepperController sc = new StepperController(); 
 		while (stop != true) {
 			try {
-				while(SensorExamples.readPressure() != peep) {
+				float p1 = SensorExamples.readPressure();
+				Thread.sleep(10);
+				float p2 = SensorExamples.readPressure();
+				Thread.sleep(10);
+				float p3 = SensorExamples.readPressure();
+				Thread.sleep(10);
+				float p4 = SensorExamples.readPressure();
+				Thread.sleep(10);
+				float p5 = SensorExamples.readPressure();
+				Thread.sleep(10);
+				float avgp = (p1 + p2 + p3 + p4 + p5)/5;
+				//tweak wait times to increase/decrease breathing rate (figure out later)
+				//to think abt: will this increase or decrease precision of pressure readings?
+				
+				if(avgp > peep) {
 					sc.backward(1, 1); //think abt how to put rate and time into incremental method
-				}
-				while(SensorExamples.readPressure() != 0) {
-					sc.forward(1, 1);
+				}else {
+					if(avgp < MAX_PRESSURE) {
+						sc.forward(1, 1);
+					}
 				}
 			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
