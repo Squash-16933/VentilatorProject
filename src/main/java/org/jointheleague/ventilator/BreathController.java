36f587ec6317579cb4b5c963d02241c3d6aa1188
keithagroves <keith.groves@jointheleague.org>
2020-12-06 13:22:15 -0800

    patient profile

--- src/main/java/org/jointheleague/ventilator/BreathController.java
+++ src/main/java/org/jointheleague/ventilator/BreathController.java
@@ -1,5 +1,12 @@
 package org.jointheleague.ventilator;
 
 public class BreathController {
-
+	PatientProfile patient;
+	VentilatorSetting settings;
+	
+	public BreathController(PatientProfile patientProfile) {
+		
+	}
+	
+	
 }
