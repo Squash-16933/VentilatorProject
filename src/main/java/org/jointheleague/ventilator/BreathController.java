844e0e8aebbe102f61ae3a042431b0c24ba3d3cd
daliasebat <daliasebat@gmail.com>
2020-12-13 13:53:59 -0800

    refactoring

--- src/main/java/org/jointheleague/ventilator/BreathController.java
+++ src/main/java/org/jointheleague/ventilator/BreathController.java
@@ -1,12 +1,22 @@
 package org.jointheleague.ventilator;
 
+import org.jointheleague.ventilator.sensors.Peep;
+import org.jointheleague.ventilator.stepper.StepperController;
+
 public class BreathController {
+	StepperController sc = new StepperController();
 	PatientProfile patient;
 	VentilatorSetting settings;
-	
+	Peep p = new Peep();
 	public BreathController(PatientProfile patientProfile) {
 		
 	}
 	
+	public void pressureCheck() {
+		String status = p.runPeep(settings.getPeep());
+		if(status.equals(Peep.FORWARD)) {
+			sc.forwardStep(); //change?
+		}
+	}
 	
 }
--- src/main/java/org/jointheleague/ventilator/sensors/Peep.java
+++ src/main/java/org/jointheleague/ventilator/sensors/Peep.java
@@ -9,44 +9,43 @@ import org.jointheleague.ventilator.stepper.StepperController;
 
 public class Peep {
 	public static final int MAX_PRESSURE = 5; // fix this lol
-
-	public static void runPeep(float peep) {
-		float avgp = 0;
-		boolean stop = false;
-		StepperController sc = new StepperController();
-		ArrayList<Float> pressures = new ArrayList<Float>();
-
-		while (stop != true) {
+	public static final String FORWARD = "forward";
+	public static final String BACKWARD = "backward";
+	public static final String NOT_READY = "not ready";
+	public ArrayList<Float> pressures = new ArrayList<Float>();
+	public double avgp = 0;
+	public String runPeep(double peep) {
+		
 			try {
 				if (pressures.size() >= 5) {
+					pressures.remove(0);
+					pressures.add(SensorExamples.readPressure());
 					for (int i = 0; i < pressures.size(); i++) {
 						avgp = avgp + pressures.get(i);
 					}
 					avgp = avgp / 5;
 
 					if (avgp > peep) {
-						sc.backwardStep();
+						//sc.backwardStep();
+						return BACKWARD;
 					} else {
 						if (avgp < MAX_PRESSURE) {
-							sc.forwardStep();
+							//sc.forwardStep();
+							return FORWARD;
 						}
 					}
 
-					pressures.remove(0);
-					pressures.add(SensorExamples.readPressure());
 				} else {
 					pressures.add(SensorExamples.readPressure());
-				}
-
-				if (1 == 0) { // stop machine or change setting
-					stop = true;
+					return NOT_READY;
 				}
 
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			return null;
 		}
 
 	}
-}
+//}
