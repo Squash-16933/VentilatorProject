23279488e37925ecf1ca2686ddd2ee1195dedd86
Fangtastic7 <mwfang7@gmail.com>
2020-12-20 14:15:05 -0800

    VentilatorModes class w/modes

---
+++ src/main/java/org/jointheleague/ventilator/VentilatorModes.java
@@ -0,0 +1,9 @@
+package org.jointheleague.ventilator;
+
+public class VentilatorModes {
+//Volume  Assist Control (ACV)
+//SIMV - irregular breaths and adjusts based on patient
+//Pressure-Controlled Ventilation (PCV)
+//Pressure Support Ventilation (PSV) 
+//Dynamic Mode
+}
