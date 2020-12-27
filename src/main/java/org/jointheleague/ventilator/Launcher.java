9747b228319f98ed502827b7d5d81c0dda6ebc77
keithagroves <keith.groves@jointheleague.org>
2020-12-27 14:18:32 -0800

    change ports

--- src/main/java/org/jointheleague/ventilator/Launcher.java
+++ src/main/java/org/jointheleague/ventilator/Launcher.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Launcher {
+	public static final int LOCAL_PORT = 5001;
 	public static void main(String[] args) {
 		SpringApplication.run(Launcher.class, args);
-		VentilatorController e = new VentilatorController(3001);
+		VentilatorController e = new VentilatorController(LOCAL_PORT);
 		e.start();
 	}
 }
