ce606dd97c711f82f2abd7f438728ee35bb13808
Fangtastic7 <mwfang7@gmail.com>
2021-03-13 14:03:27 -0800

    VentilatorModes

--- src/main/java/org/jointheleague/ventilator/VentilatorModes.java
+++ src/main/java/org/jointheleague/ventilator/VentilatorModes.java
@@ -13,6 +13,8 @@ public class VentilatorModes {
 	VentilatorCalcs vc;
 	String VentilatorMode;
 	double IntraPress;
+	BreathController bc;
+	String phase = "inspiratory";
 	// brainstorm
 	/*
 	 * What I need: volume of breath per interval of time = tidal vol/inspiratory
@@ -22,31 +24,36 @@ public class VentilatorModes {
 	 * delivery
 	 * 
 	 */
-public VentilatorModes(VentilatorSetting vs, VentilatorCalcs vc, String VentilatorMode, double IntraPress) {
+public VentilatorModes(VentilatorSetting vs, VentilatorCalcs vc, String VentilatorMode, double IntraPress, BreathController bc) {
 	this.vs = vs;
 	this.vc = vc;
+	//bc = new BreathController();
 //	this.VentilatorMode = vc.ven
 }
 	// Volume Assist Control
 	public void VolumeAssist() {
 		double ftdv = (vs.getMaxTidalVolume()) / (vc.inspPhaseTime()); //fixed tidal volume given at specific intervals
 		double atv = 0; //accumulated tidal volume
-		while (VentilatorMode.equals("Volume Assist Control")) {
+		if (VentilatorMode.equals("Volume Assist Control")) {
 			try {
-				Thread.sleep(Double.valueOf(vc.inhaleTime()).longValue());
-				// push the ftdv into patient - how????
+				//delay for inhale time
+				//step forward steppercontroller to input air (compress)
+				//Thread.sleep(Double.valueOf(vc.inhaleTime()).longValue());
+				//or once that volume is pushed to patient, continue to inhale
+				//change phase to expiratory
 				atv = atv + ftdv;
-			} catch (InterruptedException e) {
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			if (atv == vs.getMaxTidalVolume()) { // tells us that all the air in a cycle is delivered to the patient
+			if (phase.equals("expiratory") || atv == vs.getMaxTidalVolume()) { // tells us that all the air in a cycle is delivered to the patient
 				// go to expiratory which means to pause this code and activate peep
 				try {
-					//activate expiratory phase and peep
-					Thread.sleep(Double.valueOf(vc.expirPhaseTime()).longValue());
+					//activate peep, stop giving patient air, step backward?
+					//Thread.sleep(Double.valueOf(vc.expirPhaseTime()).longValue());
+					//keep running for a certain amount of time based off I&E ratio
 					atv = 0;
-				} catch (InterruptedException e) {
+				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
