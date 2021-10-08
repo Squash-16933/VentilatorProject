2e1120a460a2292990427d320e0b96e4d6aff25a
keithagroves <keith.groves@jointheleague.org>
2021-10-07 17:11:45 -0700

    Update StepperController.java

--- src/main/java/org/jointheleague/ventilator/stepper/StepperController.java
+++ src/main/java/org/jointheleague/ventilator/stepper/StepperController.java
@@ -62,7 +62,7 @@ public class StepperController implements StepperInterface {
 	}
 
 	public void backward(double rate, double time) {
-		pin15.high();// EN invalid, motor under control
+		pin15.low();// EN invalid, motor under control
 		{
 			pin11.low();// DIR backward
 			for (int k = 0; k < rate * time; k++) {
