89ee3b2c691235401f8311f04efe70d195f05028
dalidog <daliasebat@gmail.com>
2021-10-17 13:29:51 -0700

    Update Test.java

--- src/main/java/org/jointheleague/test/Test.java
+++ src/main/java/org/jointheleague/test/Test.java
@@ -64,14 +64,14 @@ public class Test {
 		StepperInterface sc = new StepperController();
 		//pc.moveToTop(sc);
 		for(int i = 0; true; i++) {
- 			sc.forward(300, 10);
+ 			sc.forward(500, 10);
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 
-			sc.backward(300,10);
+			sc.backward(500,10);
 			try {
 				Thread.sleep(200);
 			} catch (InterruptedException e) {
