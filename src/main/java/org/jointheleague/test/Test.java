1d79379b71af67fc36edd75bbe95cb335f4726ca
WriterArtistCoder <tinystripz@outlook.com>
2022-02-20 13:42:54 -0800

    Added LIDAR test as new thread

--- src/main/java/org/jointheleague/test/Test.java
+++ src/main/java/org/jointheleague/test/Test.java
@@ -11,30 +11,31 @@ import org.jointheleague.ventilator.BreathController;
 import org.jointheleague.ventilator.PatientProfile;
 import org.jointheleague.ventilator.PositionCheck;
 import org.jointheleague.ventilator.sensors.SensorReader;
-import sun.misc.Signal;
+import sun.misc.Signal;1
+
 /**
  * Tests the stepper controller, moves motor back and forth.
  */
 
 // TODO add javadocs
 public class Test {
-	
+
 	public static double startTime = System.currentTimeMillis();
+
 	@SuppressWarnings("restriction")
 	public Test(String... args) {
-		
 
-		System.out.println("running simple tests v102");
-		//setPin();
-		if(args.length > 0){
-			System.out.println("Running ventilator at " + args[0] + " steps per second for " + args[1] + " seconds.");	
+		System.out.println("running simple tests v20220220A");
+		// setPin();
+		if (args.length > 0) {
+			System.out.println("Running ventilator at " + args[0] + " steps per second for " + args[1] + " seconds.");
 			simpleStepperTest(args);
-		}else{
-			//simpleStepperTest();
-			lidarTest();	
-			//pressureTest();
+		} else {
+			// simpleStepperTest();
+			lidarTest();
+			// pressureTest();
 		}
-		
+
 	}
 
 	private void setPin() {
@@ -42,25 +43,25 @@ public class Test {
 		StepperInterface sc = new StepperController();
 		sc.setPin15High();
 	}
-	
+
 	private void pressureTest() {
 		SensorReader sr = new SensorReader();
-		while(1==1) {
+		while (1 == 1) {
 			System.out.println("PRESSURE: " + sr.readPressure());
 		}
 	}
 
 	private void lidarTest() {
 		SensorReader sr = new SensorReader();
-		while(1==1) {
+		while (1 == 1) {
 			System.out.println("LIDAR: " + sr.readLidar());
 		}
 	}
-	
+
 	private void comprehensiveStepperTest() {
 		// TODO Auto-generated method stub
 		PositionCheck pc = new PositionCheck(new SensorReader());
-		PatientProfile testP = new PatientProfile(16,(double)64,(double)120,(double)20.6,"female","COVID-19");
+		PatientProfile testP = new PatientProfile(16, (double) 64, (double) 120, (double) 20.6, "female", "COVID-19");
 		BreathController bc = new BreathController(testP);
 		StepperInterface sc = bc.getStepperController();
 		pc.moveToTop(sc);
@@ -68,37 +69,43 @@ public class Test {
 		Timer t = new Timer();
 		TimerTask tt = new TimerTask2(lidarNumbers);
 		t.scheduleAtFixedRate(tt, new Date(System.currentTimeMillis()), 500);
-		while (1==1) {
+		while (1 == 1) {
 			bc.breathe();
-			//use calculateBreathRate() here whenever you need to display or use it?
-			//System.out.println("RESP RATE: " + bc.calculateRespRate(((TimerTask2) tt).getLidarVals()));
+			// use calculateBreathRate() here whenever you need to display or use it?
+			// System.out.println("RESP RATE: " + bc.calculateRespRate(((TimerTask2)
+			// tt).getLidarVals()));
 			((TimerTask2) tt).getLidarVals();
-			
+
 		}
 	}
 
 	void simpleStepperTest() {
-		
-		
-		//PositionCheck pc = new PositionCheck(new SensorReader());
+
+		// PositionCheck pc = new PositionCheck(new SensorReader());
 		StepperInterface sc = new StepperController();
-		//pc.moveToTop(sc);
-		Signal.handle(new Signal("INT"),  // SIGINT
-			    signal -> {
-			    	System.out.println("ctrl c pressed");
-			    	sc.stop();
-			    	System.exit(0);
-			    });
-		for(int i = 0; i<5; i++) {
+		// pc.moveToTop(sc);
+
+		Runnable r = () -> lidarTest();
+
+		Thread t = new Thread(r);
+		t.start();
+
+		Signal.handle(new Signal("INT"), // SIGINT
+				signal -> {
+					System.out.println("ctrl c pressed");
+					sc.stop();
+					System.exit(0);
+				});
+		for (int i = 0; i < 5; i++) {
 			System.out.println("running forward");
- 			sc.forward(600, 100);
+			sc.forward(600, 100);
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			System.out.println("running backward");
- 			sc.backward(600,10);
+			sc.backward(600, 10);
 			try {
 				Thread.sleep(200);
 			} catch (InterruptedException e) {
@@ -107,22 +114,22 @@ public class Test {
 		}
 		sc.stop();
 		System.out.println("stopped ventilator");
-		}
-	
+	}
+
 	void simpleStepperTest(String[] args) {
 		String direction = args[0];
 		int rate = Integer.parseInt(args[1]);
 		int time = Integer.parseInt(args[2]);
 		StepperInterface sc = new StepperController();
-		Signal.handle(new Signal("INT"),  // SIGINT
-			    signal -> {
-			    	System.out.println("ctrl c pressed");
-			    	sc.stop();
-			    	System.exit(0);
-			    });
-		if(direction.equals("forward")){
-			for( ; ; ) {
-				//down first
+		Signal.handle(new Signal("INT"), // SIGINT
+				signal -> {
+					System.out.println("ctrl c pressed");
+					sc.stop();
+					System.exit(0);
+				});
+		if (direction.equals("forward")) {
+			for (;;) {
+				// down first
 				sc.forward(rate, time);
 				try {
 					Thread.sleep(500);
@@ -137,9 +144,9 @@ public class Test {
 				}
 
 			}
-		}else{
-			for( ; ; ) {
-				//up first
+		} else {
+			for (;;) {
+				// up first
 				sc.backward(rate, time);
 				try {
 					Thread.sleep(500);
@@ -154,14 +161,15 @@ public class Test {
 				}
 
 			}
-			
+
 		}
-}}
+	}
+}
 
-class TimerTask2 extends TimerTask{
+class TimerTask2 extends TimerTask {
 	double[] lidNums;
 	SensorReader sr = new SensorReader();
-	
+
 	public TimerTask2(double[] ln) {
 		lidNums = new double[ln.length];
 	}
@@ -169,16 +177,16 @@ class TimerTask2 extends TimerTask{
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		for(int i = 1; i < lidNums.length; i++) {
-			lidNums[i-1]=lidNums[i];
+		for (int i = 1; i < lidNums.length; i++) {
+			lidNums[i - 1] = lidNums[i];
 		}
-		lidNums[lidNums.length-1]= sr.readLidar();
-		System.out.println("READING LIDAR: "+ lidNums[lidNums.length-1]);
+		lidNums[lidNums.length - 1] = sr.readLidar();
+		System.out.println("READING LIDAR: " + lidNums[lidNums.length - 1]);
 	}
-	
-	public double[] getLidarVals(){
+
+	public double[] getLidarVals() {
 		return lidNums;
 	}
-	//mvn command mvn spring-boot:run -Dspring-boot.run.arguments="forward 500 200"
-	
+	// mvn command mvn spring-boot:run -Dspring-boot.run.arguments="forward 500 200"
+
 }
